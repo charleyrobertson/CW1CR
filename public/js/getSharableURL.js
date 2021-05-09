80651567c71381935f05f602d2b352de270a7258
CharleyRobertson <weechazaaa1@gmail.com>
2021-05-09 18:59:39 +0100

    Changing H5 to LI

--- public/js/getSharableURL.js
+++ public/js/getSharableURL.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
     let url = window.location.href.toString();
-    var h5 = document.createElement("h5");
+    var li = document.createElement("li");
     var div = document.getElementById("shareLink");
     var date = new Date().toISOString().substring(0,10);
     url = url.concat("/", date);
     
-    h5.innerHTML = url;
+    li.innerHTML = url;
 
     div.append(h5);
 });
